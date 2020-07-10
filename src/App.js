4ed74929b80c7db6e3795cc3dafc6e95f3f95465
An Jaemyeong <jae@Anui-MacBookPro.local>
2020-07-10 23:59:35 +0900

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./Component/customer";
+
+const customers = [
+  {
+    id: 1,
+    img: "https://placeimg.com/64/64/1",
+    name: "Jane",
+    bday: "560711",
+    gender: "Female",
+    job: "Marketing",
+  },
+  {
+    id: 2,
+    img: "https://placeimg.com/64/64/2",
+    name: "Alex",
+    bday: "560711",
+    gender: "Female",
+    job: "Designer",
+  },
+  {
+    id: 3,
+    img: "https://placeimg.com/64/64/3",
+    name: "Poo",
+    bday: "560711",
+    gender: "Male",
+    job: "Dev",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <p>TEST </p>
+    <div className="background">
+      {customers.map((customer) => {
+        return (
+          <Customer
+            key={customer.id}
+            id={customer.id}
+            img={customer.img}
+            name={customer.name}
+            bday={customer.bday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        );
+      })}
     </div>
   );
 }
